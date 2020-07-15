2dc1f4409d8d37389c2a74c91932b85c1336b4ff
Olga Semykina <semikinaolga@mail.ru>
2020-07-15 10:25:14 +0300

    edit Bonus.java

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,7 +1,7 @@
 public class Bonus {
     public static void main(String[] args) {
         int paymentKop=10000;
-        int bonus=paymentKop /100/100;
+        int bonus=paymentKop/100/100;
         int balance=200;
         int limitKop=100000;
         if (paymentKop<limitKop) {
