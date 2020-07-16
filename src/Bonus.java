f93e6655362540a0187a65137ddcc150855d17c2
Olga Semykina <semikinaolga@mail.ru>
2020-07-16 09:34:04 +0300

    edit space

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,11 +1,11 @@
 public class Bonus {
     public static void main(String[] args) {
-        int paymentKop=10000;
-        int bonus=paymentKop/100/100;
-        int balance=200;
-        int limitKop=100000;
-        if (paymentKop<limitKop) {
-            bonus=0;
+        int paymentKop = 10000;
+        int bonus = paymentKop / 100 / 100;
+        int balance = 200;
+        int limitKop = 100000;
+        if (paymentKop < limitKop) {
+            bonus = 0;
         }
         System.out.println(bonus);
     }
